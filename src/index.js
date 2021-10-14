09f0f620ca91cc869ac28bb93eb7f77b8001494d
Christopher <christopher.diercksen@gmail.com>
2021-10-14 11:42:20 -0400

    tested index.js connection

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+console.log("hello world")
 // EventListener 1 - User clicks a Best Seller List Category button and it fetches from New York Times Books API
 // Display list of books on current Best Seller list for selected category
 // EventListener 2 - user can click a title and the review will be displayed
