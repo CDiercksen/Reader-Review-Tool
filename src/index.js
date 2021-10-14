c9746c5c6bf1bb17b722cdc089a6dd750a9c8b0e
Christopher <christopher.diercksen@gmail.com>
2021-10-14 12:07:08 -0400

    expanded pseudocode in index.js; set fictionButton variable to html id

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 console.log("hello world")
 // EventListener 1 - User clicks a Best Seller List Category button and it fetches from New York Times Books API
+    //grab buttons from html and assign to variables
+    const fictionButton = document.querySelector("#get-fiction")
+    //add event listener for click event
+    //save API address as variable
+    //make function to make GET requests from API
+    
 // Display list of books on current Best Seller list for selected category
+
 // EventListener 2 - user can click a title and the review will be displayed
+
 // EventListener 3 - user can click a heart icon and favorite the book
