92bd405f9cca484fcd6664ac75634c00080cfc9f
Christopher <christopher.diercksen@gmail.com>
2021-10-14 14:25:10 -0400

    successfully fetched data for fetchFiction

--- src/index.js
+++ src/index.js
@@ -2,20 +2,21 @@ console.log("hello world")
 // EventListener 1 - User clicks a Best Seller List Category button and it fetches from New York Times Books API
     //grab buttons from html and assign to variables
     const fictionButton = document.querySelector("#get-fiction")
-    //add event listener for click event
-    fictionButton.addEventListener('click', fetchFiction())
     //save API address as variable
-    const apiFiction = "https://api.nytimes.com/svc/books/v3/lists.json"
+    const apiFiction = "https://api.nytimes.com/svc/books/v3/lists/hardcover-fiction.json"
     const apiKey = "?api-key=UF8xmakry6jkkIvUvg7AqjHObjVGD9y2"
     //make function to make GET requests from API
-    const fetchFiction = () => {
-        fetch(apiFiction, apiKey)
+    function fetchFiction() {
+       fetch("https://api.nytimes.com/svc/books/v3/lists/hardcover-fiction.json?api-key=UF8xmakry6jkkIvUvg7AqjHObjVGD9y2")
+        // fetch(apiFiction, apiKey)
         .then(res => res.json())
         .then(data => {
             console.log(data)
         })
     }
     
+    //add event listener for click event
+    fictionButton.addEventListener('click', fetchFiction())
 // Display list of books on current Best Seller list for selected category
 
 // EventListener 2 - user can click a title and the review will be displayed
