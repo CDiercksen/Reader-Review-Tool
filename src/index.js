a12347295b4110a4c48fdbd380cdf4c3816dd120
Christopher <christopher.diercksen@gmail.com>
2021-10-22 12:03:07 -0400

    added DomContentLoaded; made list items links by setting li.innerHTML

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
-console.log("hello world")
-// EventListener 1 - User clicks a Best Seller List Category button and it fetches from New York Times Books API
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("hello DOM")
+    
+})
+    // EventListener 1 - User clicks a Best Seller List Category button and it fetches from New York Times Books API
     //grab buttons from html and assign to variables
     const fictionButton = document.querySelector("#get-fiction")
     const nonFictionButton = document.querySelector("#get-non-fiction")
@@ -28,19 +31,25 @@ console.log("hello world")
     //add event listener for click event
     fictionButton.addEventListener('click', () => fetchFiction())
     nonFictionButton.addEventListener('click', () => fetchNonFiction())
-// Display list of books on current Best Seller list for selected category
-function displayList(books) {
-    const main = document.querySelector("#review-list")
-    books.forEach(bookObject => {
-        const li = document.createElement('li');
-        li.innerText= bookObject.title + ", by " + bookObject.author;
-        debugger
-    main.appendChild(li);
-    })
-}
-// EventListener 2 - user can click a title and the review will be displayed
-
-// EventListener 3 - user can click a heart icon and favorite the book
+    // Display list of books on current Best Seller list for selected category
+    function displayList(books) {
+        const main = document.querySelector("#review-list")
+        //if #review-list has > 0 li, remove them from the DOM
+        books.forEach(bookObject => {
+            const li = document.createElement('li');
+            // debugger
+            li.id = "item" + bookObject.rank
+            li.innerHTML = `<a href ${bookObject.sunday_review_link}>${bookObject.title + ", by " + bookObject.author}</a>`;
+            //make inner text a link to bookObject.sunday_review_link
+            // add heart icon
+            main.appendChild(li);
+        })
+    }
+    // EventListener 2 - user can click a title and the description will be displayed
+    
+    // EventListener 3 - user can click a heart icon and favorite the book
+    // write a function that listens for a click on the heart icon, and copies the li into #saved-review-container
+    
 
 //get bootstrap for css
 //learn how to use bootstrap grid for layouts
